13974278b07e0cdc9b943988575f563348ef9388
tasuku <tasuku1215mhg@gmail.com>
2019-07-05 09:13:11 +0900

    開発時CSS無効＋フォーマット

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,19 +1,19 @@
-$(function() {
+$(function () {
 
     var fileArea = document.getElementById('drag-drop-area');
     var fileInput = document.getElementById('fileInput');
 
 
-    fileArea.addEventListener('dragover', function(evt){
-      evt.preventDefault();
-      fileArea.classList.add('dragover');
+    fileArea.addEventListener('dragover', function (evt) {
+        evt.preventDefault();
+        fileArea.classList.add('dragover');
     });
 
-    fileArea.addEventListener('dragleave', function(evt){
+    fileArea.addEventListener('dragleave', function (evt) {
         evt.preventDefault();
         fileArea.classList.remove('dragover');
     });
-    fileArea.addEventListener('drop', function(evt){
+    fileArea.addEventListener('drop', function (evt) {
         evt.preventDefault();
         fileArea.classList.remove('dragenter');
         var files = evt.dataTransfer.files;
--- public/upload.php
+++ public/upload.php
@@ -1,49 +1,51 @@
 <!DOCTYPE html>
 <html lang="ja">
-    <head>
-        <meta charset="UTF-8">
-        <title>UPLOAD</title>
-        <link rel="stylesheet" type="text/css" href="./css/upload.css">
-        <script src="../public/js/jquery-3.3.1.min.js"></script>
-        <script src="../public/js/upload.js"></script>
-    </head>
-    <body>
-        <header>
-            <h1>UPLOAD</h1>
-        </header>
+<head>
+    <meta charset="UTF-8">
+    <title>UPLOAD</title>
+<!--    <link rel="stylesheet" type="text/css" href="./css/upload.css">-->
+    <script src="../public/js/jquery-3.3.1.min.js"></script>
+    <script src="../public/js/upload.js"></script>
+</head>
+<body>
+<header>
+    <h1>UPLOAD</h1>
+</header>
 
-        <form action="" method="post" enctype="multipart/form-data">
-            <div id="drag-drop-area">
-                <div class="drag-drop-inside">
-                    <p class="drag-drop-info">Drop file</p>
-                    <p>or</p>
-                        Send these files:
-                        <input id="fileInput" type="file" name="upfile[]" multiple><br/><br/>
-                        <!--        <input type="file" name="upfile[]"/><br/>-->
-                        <input type="submit" value="Send files"/>
-                </div>
-            </div>
-        </form>
-    </body>
-<html>
-<?php
-if (!empty($_FILES)) {
-    for ($i = 0; $i < count($_FILES["upfile"]["tmp_name"]); $i++) {
-        if (is_uploaded_file($_FILES["upfile"]["tmp_name"][$i])) {
-            $fileName = "files/" . date("[Y-m-d_His]") . $_FILES["upfile"]["name"][$i];
-            if (file_exists($fileName) === false) {
-                if (move_uploaded_file($_FILES["upfile"]["tmp_name"][$i], $fileName)) {
-                    chmod($fileName, 0644);
-                    echo $_FILES["upfile"]["name"][$i] . "をアップロードしました。<br>";
+<form action="" method="post" enctype="multipart/form-data">
+
+    <?php
+    if (!empty($_FILES)) {
+        for ($i = 0; $i < count($_FILES["upfile"]["tmp_name"]); $i++) {
+            if (is_uploaded_file($_FILES["upfile"]["tmp_name"][$i])) {
+                $fileName = "files/" . date("[Y-m-d_His]") . $_FILES["upfile"]["name"][$i];
+                if (file_exists($fileName) === false) {
+                    if (move_uploaded_file($_FILES["upfile"]["tmp_name"][$i], $fileName)) {
+                        chmod($fileName, 0644);
+                        echo $_FILES["upfile"]["name"][$i] . "をアップロードしました。<br>";
+                    } else {
+                        echo "アップロードエラー";
+                    }
                 } else {
-                    echo "アップロードエラー";
+                    echo "既にファイルが存在します。少し時間をおいてやり直してください。";
                 }
             } else {
-                echo "既にファイルが存在します。少し時間をおいてやり直してください。";
+                echo "ファイルが選択されていません";
             }
-        } else {
-            echo "ファイルが選択されていません";
         }
     }
-}
-?>
+    ?>
+
+    <div id="drag-drop-area">
+        <div class="drag-drop-inside">
+            <p class="drag-drop-info">Drop file</p>
+            <p>or</p>
+            Send these files:
+            <input id="fileInput" type="file" name="upfile[]" multiple><br/><br/>
+            <!--        <input type="file" name="upfile[]"/><br/>-->
+            <input type="submit" value="Send files"/>
+        </div>
+    </div>
+</form>
+</body>
+<html>
