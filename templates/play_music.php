cfcba989264810d08fc979d577fe29853e5cab7c
kaitotaku7 <kaitotaku7@gmail.com>
2019-07-05 11:52:32 +0900

    音楽再生画面html

---
+++ templates/play_music.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="">
+    <title>音楽再生画面</title>
+    <link rel="stylesheet" type="text/css" href="./css/design.css">
+</head>
+<body>
+  <article id="play_music">
+  <img src="./../img/真顔.png" alt="" id="neutral">
+  <h2>Lemon</h2>
+</article>
+<article id="contlol">
+<img src="./../img/巻き戻し.png" width="50">
+<img src="./../img/再生.png" width="60">
+<img src="./../img/早送り.png" width="50">
+</article>
+<article id="music_list">
+  <ul>
+    <li>Lemon　　　　　　　　　　　米津玄師　　　　　　〇</li>
+    <li>マリーゴールド　　　　　　　あいみょん　　　　　〇</li>
+    <li>シャルル　　　　　　　　　　バルーン　　　　　　〇</li>
+    <li>さよならエレジー　　　　　　菅田将暉　　　　　　〇</li>
+    <li>残酷な天使のテーゼ　　　　　高橋洋子　　　　　　〇</li>
+  </ul>
+</article>
+</body>
+</html>
